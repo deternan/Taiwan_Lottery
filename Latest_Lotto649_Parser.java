87b5e9bd48a27f701686de5f58f4d2cde8424e82
deternan <chke@iir.csie.ncku.edu.tw>
2017-07-30 01:31:39 +0800

    Lotto649

--- Latest_Lotto649_Parser.java
+++ Latest_Lotto649_Parser.java
@@ -6,12 +6,19 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 /*
+ * Taiwan Lotto 649 (Â§ßÊ®ÇÈÄè)
+ * 
+ * version: June 05, 2013	03:30 PM
+ * Last revision: July 30, 2017 00: 42 PM
+ */
+
+/*
  * Jar
  * jsoup-1.10.1.jar 
  * json-simple-1.1.1
  */
 
-public class Lottery_649_Parser 
+public class Latest_Lotto649_Parser 
 {
 	// Taiwan Lottery URL
 	private String url = "http://www.taiwanlottery.com.tw/lotto/Lotto649/history.aspx";
@@ -24,17 +31,16 @@ public class Lottery_649_Parser
 	// Json output
 	private JSONObject obj;
 	
-	public Lottery_649_Parser() throws Exception
+	public Latest_Lotto649_Parser() throws Exception
 	{
-		Document doc = Jsoup.connect(url).get();		
-		//Elements links = doc.select("table[class]");
-		Elements links = doc.select("#Lotto649Control_history_dlQuery");			
+		Document doc = Jsoup.connect(url).get();				
+		Elements links = doc.select("#Lotto649Control_history_dlQuery");					
 		
-		// ¥¡ßO		
-		ID_number = links.select("#Lotto649Control_history_dlQuery_L649_DrawTerm_0").text();				
-		// ∂}º˙§È
+		// ID
+		ID_number = links.select("#Lotto649Control_history_dlQuery_L649_DrawTerm_0").text();						
+		// Date
 		date_str = links.select("#Lotto649Control_history_dlQuery_L649_DDate_0").text();
-		// §j§p∂∂ß«
+		// ÔøΩjÔøΩpÔøΩÔøΩÔøΩÔøΩ
 		number_array = new int[7];
 		number_array[0] = Integer.parseInt(links.select("#Lotto649Control_history_dlQuery_No1_0").text());
 		number_array[1] = Integer.parseInt(links.select("#Lotto649Control_history_dlQuery_No2_0").text());
@@ -43,7 +49,7 @@ public class Lottery_649_Parser
 		number_array[4] = Integer.parseInt(links.select("#Lotto649Control_history_dlQuery_No5_0").text());
 		number_array[5] = Integer.parseInt(links.select("#Lotto649Control_history_dlQuery_No6_0").text());
 		number_array[6] = Integer.parseInt(links.select("#Lotto649Control_history_dlQuery_SNo_0").text());
-		// º˙™˜§¿∞t
+		// ÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩt
 		bonus_array = new int[8];
 		bonus_array[0] = Integer.parseInt(Remove_tag(links.select("#Lotto649Control_history_dlQuery_L649_CategA4_0").text()));
 		bonus_array[1] = Integer.parseInt(Remove_tag(links.select("#Lotto649Control_history_dlQuery_Label7_0").text()));
@@ -53,7 +59,7 @@ public class Lottery_649_Parser
 		bonus_array[5] = Integer.parseInt(Remove_tag(links.select("#Lotto649Control_history_dlQuery_Label11_0").text()));
 		bonus_array[6] = Integer.parseInt(Remove_tag(links.select("#Lotto649Control_history_dlQuery_Label12_0").text()));
 		bonus_array[7] = Integer.parseInt(Remove_tag(links.select("#Lotto649Control_history_dlQuery_Label13_0").text()));
-		// §§º˙™`º∆
+		// ÔøΩÔøΩÔøΩÔøΩÔøΩ`ÔøΩÔøΩ
 		unit_array = new int[8];
 		unit_array[0] = Integer.parseInt(Remove_tag(links.select("#Lotto649Control_history_dlQuery_L649_CategA3_0").text()));
 		unit_array[1] = Integer.parseInt(Remove_tag(links.select("#Lotto649Control_history_dlQuery_L649_CategB3_0").text()));
@@ -64,7 +70,8 @@ public class Lottery_649_Parser
 		unit_array[6] = Integer.parseInt(Remove_tag(links.select("#Lotto649Control_history_dlQuery_Label5_0").text()));
 		unit_array[7] = Integer.parseInt(Remove_tag(links.select("#Lotto649Control_history_dlQuery_Label6_0").text()));
 		
-		Output_JSON();;		
+		Output_JSON();
+		System.out.println(obj);
 	}
 	
 	private void Output_JSON()
@@ -86,12 +93,10 @@ public class Lottery_649_Parser
 			list_bonus.add(bonus_array[i]);
 			list_unit.add(unit_array[i]);			
 		}
-		
-		
+				
 		obj.put("Number", list_number);
 		obj.put("bouns", list_bonus);
-		obj.put("Unit", list_unit);
-				
+		obj.put("Unit", list_unit);			
 	}
 	
 	private String Remove_tag(String input_str)
@@ -110,7 +115,7 @@ public class Lottery_649_Parser
 	public static void main(String[] args)
 	{
 		try {
-			Lottery_649_Parser LP = new Lottery_649_Parser();
+			Latest_Lotto649_Parser LP = new Latest_Lotto649_Parser();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
