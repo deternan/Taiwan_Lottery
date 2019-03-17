663c157f544f401a161120d6c1dd9cb4d9bd99d8
Chao-Hsuan Ke <chke@iir.csie.ncku.edu.tw>
2019-03-17 12:50:13 +0800

    parsing number (latest)

--- src/main/java/com/taiwan/jsopParser.java
+++ src/main/java/com/taiwan/jsopParser.java
@@ -1,17 +1,19 @@
 package com.taiwan;
 
+import java.io.IOException;
+
 /*
- * Bobble Sort (Array)
+ * Jsoup Prser
  * 
  * version: March 11, 2019 10:01 PM
- * Last revision: March 11, 2019 11:26 PM
+ * Last revision: March 17, 2019 12:10 PM
  * 
  */
 
 /*
  * Reference
  * https://blog.csdn.net/sbsujjbcy/article/details/44853967
- * 
+ * https://www.jianshu.com/p/5277cea9d3ac
  */
 
 import org.jsoup.Jsoup;
@@ -31,16 +33,29 @@ public class jsopParser {
 	
 	private void Prser_649() throws Exception{
 		
-        String html = Jsoup.connect(pageURL).get().html();
-        //System.out.println(html);
+        // get Num
+		getBallNum();
+        
+        
+	}
+	
+	private void getBallNum() throws Exception
+	{
+		String html = Jsoup.connect(pageURL).get().html();
         
         Document doc = Jsoup.connect(pageURL).get();
         Elements spanId = doc.select("span");
 		
         // latest date
         System.out.println(doc.getElementById("latest_date").text());
+        // Number
         
-        
+        for(int ballNum=0; ballNum < doc.getElementsByClass("ball").size(); ballNum++)
+        {
+        	System.out.println(doc.getElementsByClass("ball").get(ballNum).attr("title"));
+        	
+        	//System.out.println(doc.getElementsByClass("ball").attr("title"));
+        }
 	}
 	
 	public static void main(String[] args) {
